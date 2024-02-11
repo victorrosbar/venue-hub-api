e72ce58ac40801b88bcec0f127bb2a50fc21a71c
Victor Barros <victorbarros.tec@ciclic.com.br>
2024-02-11 17:18:43 +1100

    Feat: Initial commit

---
+++ src/main/java/com/vtech/venuehubapi/VenueHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.vtech.venuehubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenueHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenueHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtech/venuehubapi/VenueHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtech.venuehubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenueHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
